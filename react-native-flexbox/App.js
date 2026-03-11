7611fa5ae9f6e10d86aedda05a5c36462baf84ec
Ruth May Regino <ruthmayreginos2786@gmail.com>
2026-03-11 19:54:15 +0800

    Done with layout direction section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -1,11 +1,15 @@
 import FlexScreen from './src/screens/FlexScreen';
+import FlexDirectionScreen from './src/screens/FlexDirectionScreen';
+import LayoutDirectionScreen from './src/screens/LayoutDirectionScreen';
 
 export default function App() {
   return <FlexScreen />;
 }
 
-import FlexDirectionScreen from './src/screens/FlexDirectionScreen';
-
 export default function App() {
   return <FlexDirectionScreen />;
+}
+
+export default function App() {
+  return <LayoutDirectionScreen />;
 }
