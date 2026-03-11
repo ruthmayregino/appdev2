740bd8ba8f51a6b1fb071839d7ab84db127a9243
Ruth May Regino <ruthmayreginos2786@gmail.com>
2026-03-11 19:25:01 +0800

    Done with flex dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,31 +1,16 @@
 import {View} from 'react-native';
 
-const FixedDimensionsBasics = () => {
+const FlexDimensionsBasics = () => {
   return (
-    <View>
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          backgroundColor: 'powderblue',
-        }}
-      />
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          backgroundColor: 'skyblue',
-        }}
-      />
-      <View
-        style={{
-          width: 150,
-          height: 150,
-          backgroundColor: 'steelblue',
-        }}
-      />
+    // Try removing the `flex: 1` on the parent View.
+    // The parent will not have dimensions, so the children can't expand.
+    // What if you add `height: 300` instead of `flex: 1`?
+    <View style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
     </View>
   );
 };
 
-export default FixedDimensionsBasics;
+export default FlexDimensionsBasics;
