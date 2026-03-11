ae61d88578ee35fe8afefa3ffe3e3e34b0e89456
Ruth May Regino <ruthmayreginos2786@gmail.com>
2026-03-11 19:24:08 +0800

    Done with fixed dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,28 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
 
-const LotsOfStyles = () => {
+const FixedDimensionsBasics = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.red}>just red</Text>
-      <Text style={styles.bigBlue}>just bigBlue</Text>
-      <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+    <View>
+      <View
+        style={{
+          width: 50,
+          height: 50,
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: 150,
+          height: 150,
+          backgroundColor: 'steelblue',
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
-export default LotsOfStyles;
+export default FixedDimensionsBasics;
